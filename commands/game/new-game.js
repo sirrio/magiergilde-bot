47520d902000a8831b6e980715022997c4e6425e
David Martin <home@sirrio.de>
2025-07-14 04:16:29 +0200

    Fix custom emoji lookup

--- commands/game/new-game.js
+++ commands/game/new-game.js
@@ -44,12 +44,15 @@ module.exports = {
         const mention = role ? `<@&${role.id}>` : '@magiergilde';
 
         const emojiMap = {
-            BT: '<:MG_BT:804713705358622800>',
-            LT: '<:MG_LT:804713705262546995>',
-            HT: '<:MG_HT:804713704918089780>',
-            ET: '<:MG_ET:804713705337782312>',
+            BT: '804713705358622800',
+            LT: '804713705262546995',
+            HT: '804713704918089780',
+            ET: '804713705337782312',
         };
-        const tierDisplay = emojiMap[tier] || tier;
+
+        const emojiId = emojiMap[tier];
+        const emoji = emojiId ? interaction.client.emojis.cache.get(emojiId) : null;
+        const tierDisplay = emoji ? emoji.toString() : tier;
 
         await interaction.reply(`${tierDisplay} - <t:${Math.floor(time / 1000)}:f> - ${text} - ${mention}`);
     },
